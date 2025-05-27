992da47ffd22ab66449dd68b07dde8faac844ef4
IMakeLists <imakelists@gmx.us>
2025-05-27 06:56:57 +0000

    questions completed

--- cohorts/2025/02-experiment-tracking/homework/hpo.py
+++ cohorts/2025/02-experiment-tracking/homework/hpo.py
@@ -39,7 +39,8 @@ def run_optimization(data_path: str, num_trials: int):
         rf.fit(X_train, y_train)
         y_pred = rf.predict(X_val)
         rmse = mean_squared_error(y_val, y_pred, squared=False)
-
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_params(params)
         return {'loss': rmse, 'status': STATUS_OK}
 
     search_space = {
--- cohorts/2025/02-experiment-tracking/homework/train.py
+++ cohorts/2025/02-experiment-tracking/homework/train.py
@@ -27,6 +27,8 @@ def run_train(data_path: str):
     y_pred = rf.predict(X_val)
 
     rmse = mean_squared_error(y_val, y_pred, squared=False)
+    print("rmse: ", rmse)
+    print("min_samples_split:", rf.min_samples_split)
 
 
 if __name__ == '__main__':
